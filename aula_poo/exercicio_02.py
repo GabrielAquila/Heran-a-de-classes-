cf3c5e867d6036625357a77d96be25023927308e
Gabriel Áquila <89168615+GabrielAquila@users.noreply.github.com>
2022-04-12 21:13:30 -0300

    Add files via upload

---
+++ aula_poo/exercicio_02.py
@@ -0,0 +1,32 @@
+# Crie a classe Animal com os atributos nome, cor e numero_patas. Crie também o método exibir_dados, que imprime na tela os dados do animal (nome, cor e numero_patas).
+# Crie a classe Cachorro que herda da classe Animal e que possui como atributo adicional a raça do cachorro. Crie também o método exibir_dados, que imprime na tela os dados do cachorro (nome, cor, numero_patas e raca)
+
+class Animal:
+    def __init__(self, nome, cor, numero_patas):
+        self.nome = nome
+        self.cor = cor
+        self.numero_patas = numero_patas
+
+    def exibir_dados(self):
+        print('---------------------')
+        print('nome:', self.nome)
+        print('cor:', self.cor)
+        print('numeros da patinhas:', self.numero_patas)
+
+
+
+class Cachorro(Animal):
+    def __init__(self, nome, cor, numero_patas, raca):
+        super().__init__(nome, cor, numero_patas)
+        self.raca = raca 
+
+    def exibir_dados(self):
+        super().exibir_dados()
+        print( 'Raça:', self.raca)
+
+animal = Animal("Passarinho", "Azul", 2)
+animal.exibir_dados()       # exibe os atributos do animal
+
+dog = Cachorro("Rex", "Marrom", 4, "Vira lata")
+dog.exibir_dados()          # exibe os atributos do cachorro
+
---
+++ aula_poo/exercicio_03.py
@@ -0,0 +1,42 @@
+# Crie a classe Imovel, que possui um endereço e um preço.
+# Crie a classe ImovelNovo, que herda de Imovel e possui um adicional no preço.
+# Crie a classe ImovelVelho, que herda de Imovel e possui um desconto no preço.
+# O método calcular_preco das classes deve retornar o preço atualizado de acordo com o adicional ou desconto. 
+
+class Imovel:
+    def __init__(self, endereco, preco):
+        self.endereco = endereco
+        self.preco = preco
+
+class ImovelVelho(Imovel):
+    def __init__(self, endereco, preco, desconto):
+        super().__init__(endereco, preco) 
+        self.desconto = desconto
+
+    def calcular_preco(self):
+        desconto = self.preco - self.desconto 
+        return desconto 
+
+class ImovelNovo(Imovel):
+    def __init__(self, endereco, preco, adicional):
+        super().__init__(endereco, preco)
+        self.adicional = adicional
+
+    def calcular_preco(self):
+        adicional = self.preco  + self.adicional
+        return adicional
+
+imovel = Imovel("Rua Silva, 123", 300000.0)
+imovel_novo = ImovelNovo("Rua Joaquim, 999", 250000.0, 20000.0)
+imovel_velho = ImovelVelho("Av. Brasil, 777", 500000.0, 35000.0)
+
+print(imovel.endereco)                                      # Rua Silva, 123
+print('Preço:', imovel.preco)                               # 300000.0
+
+print(imovel_novo.endereco)                                 # Rua Joaquim, 999
+print('Preço:', imovel_novo.preco)                          # 250000.0
+print('Preço Atualizado:', imovel_novo.calcular_preco())    # 270000.0
+
+print(imovel_velho.endereco)                                # Av. Brasil, 777
+print('Preço:', imovel_velho.preco)                         # 500000.0
+print('Preço Atualizado:', imovel_velho.calcular_preco())   # 465000.0
